f5961770c940e76ca81809737c99cf60748b927d
methuz <methuz.k@gmail.com>
2016-08-05 15:52:40 +0700

    Add new path

--- test/server.js
+++ test/server.js
@@ -13,3 +13,13 @@ describe('GET /', function() {
       .expect(200, done)
   })
 })
+
+describe('GET /hello', function() {
+  it('should response with hello', function(done) {
+    supertest(server)
+      .get('/hello')
+      .expect('Content-Type', /json/)
+      .expect('Content-Length', '27')
+      .expect(200, done)
+  })
+})
